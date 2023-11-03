317e86b140533651a81a7d8f2caa87c2f57c1d74
Kaoutar Ouadih <kaoutar.ouadih9@gmail.com>
2023-11-03 22:36:05 +0100

    add the stucture of the project + the calculation functions: addition, substraction, multiplication, division with js

---
+++ javascript.js
@@ -0,0 +1,18 @@
+function add(a, b){
+    return a + b;
+}
+
+function substract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    if(b == 0){
+        return "Math Error";
+    }
+    return a / b;
+}
