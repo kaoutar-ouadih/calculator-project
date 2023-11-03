5f2ded4b80df488f388e6e55b39d834e3fa247e4
Kaoutar Ouadih <kaoutar.ouadih9@gmail.com>
2023-11-03 22:54:25 +0100

    add the operate function which takes an operator and 2 numbers and then calls the appropriate operation type.

--- javascript.js
+++ javascript.js
@@ -16,3 +16,26 @@ function divide(a, b){
     }
     return a / b;
 }
+
+let firstNum;
+let secondNum;
+let operator;
+
+function operate(firstNum, secondNum, operator){
+    switch(operator){
+        case "+" :
+            add(firstNum, secondNum);
+            break;
+        case "-" :
+            substract(firstNum, secondNum);
+            break;
+        case "*" :
+            multiply(firstNum, secondNum);
+            break;
+        case "/" :
+            divide(firstNum, secondNum);
+            break;
+        default :
+            "No operator found!";
+    }
+}
