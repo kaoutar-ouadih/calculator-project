fa509e7163d8e341696ea271ce5e4308f3e7a2ea
Kaoutar Ouadih <kaoutar.ouadih9@gmail.com>
2023-11-05 00:56:40 +0100

    add the logic of the calculation and link the ui with the backend and handle numbers which contain multiple digits and multiple consecutive operations

--- javascript.js
+++ javascript.js
@@ -21,25 +21,75 @@ let firstNum;
 let secondNum;
 let operator;
 
-function operate(firstNum, secondNum, operator){
+function operate(firstNum, operator, secondNum){
     switch(operator){
         case "+" :
-            add(firstNum, secondNum);
+            return add(firstNum, secondNum);
             // console.log(add(firstNum, secondNum));
-            break;
         case "-" :
-            substract(firstNum, secondNum);
-            break;
+            return substract(firstNum, secondNum);
         case "*" :
-            multiply(firstNum, secondNum);
-            break;
+            return multiply(firstNum, secondNum);
         case "/" :
-            divide(firstNum, secondNum);
-            break;
+            return divide(firstNum, secondNum);
         default :
-            "No operator found!";
+            return "No operator found!";
     }
 }
+const screen = document.querySelector('#display');
+let screenDefault = document.querySelector('#display p');
+const displayValue = document.createElement('p');
+const displayResult = document.createElement('p');
+
+function display(value){
+        console.log(value);
+        if(value == '='){
+            screen.removeChild(screenDefault);
+            displayValue.textContent += ' ' ;
+            // displayValue.textContent += ' ' ;
+        }
+        else if(value == '+' || value == '-' || value == '*' || value == '/'){
+            screen.removeChild(screenDefault);
+            displayValue.textContent += ' ' + value + ' ';
+        }else{
+            screen.removeChild(screenDefault);
+            displayValue.textContent += value ;
+        }
+        screen.appendChild(displayValue);
+        screenDefault = displayValue ;//for update 
+        return displayValue.textContent;
+        
+        
+}
+const btns = document.querySelectorAll('.btn');
+
+btns.forEach((item)=>{ item.addEventListener('click', ()=>{
+        mathExp = display(item.value);
+        parsedMathExp = mathExp.split(' ');
+        console.log(parsedMathExp);
+
+        firstNum = +parsedMathExp[0];
+        operator = parsedMathExp[1];
+        secondNum = +parsedMathExp[2];
+        console.log(firstNum);
+        console.log(operator);
+        console.log(secondNum);
+
+
+} )
+})
+
+const equals = document.querySelector('#equal');
+equals.addEventListener('click', ()=>{
+    let result = operate(firstNum, operator, secondNum);
+    console.log(`The result ${result}`);
+    // display(result);
+        screen.removeChild(screenDefault);
+        displayValue.textContent = result ;
+        screen.appendChild(displayValue);
+        screenDefault = displayValue ;//for update 
+    
+})
 // let reuslt = 
 // console.log(operate(1, 5, "+"));
 // console.log(reuslt);
