3e239277a2da4e258ce5890f7970b2575305c8bc
Kaoutar Ouadih <kaoutar.ouadih9@gmail.com>
2023-11-05 22:37:06 +0100

    make the clear button works and round number in division

--- javascript.js
+++ javascript.js
@@ -14,7 +14,8 @@ function divide(a, b){
     if(b == 0){
         return "Math Error";
     }
-    return a / b;
+    resultat = a / b ;
+    return resultat.toFixed(2);
 }
 
 let firstNum;
@@ -44,9 +45,7 @@ const displayResult = document.createElement('p');
 function display(value){
         console.log(value);
         if(value == '='){
-            screen.removeChild(screenDefault);
             displayValue.textContent += ' ' ;
-            // displayValue.textContent += ' ' ;
         }
         else if(value == '+' || value == '-' || value == '*' || value == '/'){
             screen.removeChild(screenDefault);
@@ -66,30 +65,32 @@ const btns = document.querySelectorAll('.btn');
 btns.forEach((item)=>{ item.addEventListener('click', ()=>{
         mathExp = display(item.value);
         parsedMathExp = mathExp.split(' ');
-        console.log(parsedMathExp);
-
         firstNum = +parsedMathExp[0];
         operator = parsedMathExp[1];
         secondNum = +parsedMathExp[2];
-        console.log(firstNum);
-        console.log(operator);
-        console.log(secondNum);
-
 
 } )
 })
 
 const equals = document.querySelector('#equal');
 equals.addEventListener('click', ()=>{
-    let result = operate(firstNum, operator, secondNum);
-    console.log(`The result ${result}`);
-    // display(result);
+        let result = operate(firstNum, operator, secondNum);
         screen.removeChild(screenDefault);
         displayValue.textContent = result ;
         screen.appendChild(displayValue);
         screenDefault = displayValue ;//for update 
     
 })
-// let reuslt = 
-// console.log(operate(1, 5, "+"));
-// console.log(reuslt);
+
+const clear = document.querySelector('#clear');
+clear.addEventListener('click', ()=>{
+        //display nothing
+        screen.removeChild(screenDefault);
+        displayValue.textContent = null ;
+        screen.appendChild(displayValue);
+        screenDefault = displayValue ;//for update 
+        //clear all variables
+        mathExp = null;
+        parsedMathExp = [];
+    
+})
